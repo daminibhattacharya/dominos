1c4a71d3439cf8524500b85d09e3412455665f1e
Damini Bhattacharya <daminib@Daminis-MacBook-Air.fritz.box>
2024-12-17 00:25:03 +0100

    Fixed formatting

--- Dominos/Program.cs
+++ Dominos/Program.cs
@@ -5,25 +5,29 @@ public class HelloDomino
 {
     public static void Main(string[] args)
     {
-        try{
+        try
+        {
             Console.WriteLine("|._.| Welcome to Dominos |._.|");
             Console.WriteLine(" Please select the approach from the list below");
             Console.Write(" 1.Brute Force \n 2.Brute Force with Dictionaries\n 3.Graph with DFS search \nPlease press enter after input \n");
             string inputChar = Console.ReadLine();
             var input = int.Parse(inputChar);
-            if (input<0)
+            if (input < 0)
             {
                 Console.WriteLine("Invalid Input");
                 Console.Read();
                 return;
             }
-            switch(input){
-                case 1: solveByBruteForce();break;
-                case 2: solveByDictionaries();break;
+            switch (input)
+            {
+                case 1: solveByBruteForce(); break;
+                case 2: solveByDictionaries(); break;
                 case 3: solveByGraphs(); break;
-                default: Console.WriteLine("Not Implemented yet");break;
+                default: Console.WriteLine("Not Implemented yet"); break;
             }
-        }catch(Exception ex){
+        }
+        catch (Exception ex)
+        {
             Console.WriteLine("Error occured while running the app. Please try again.");
             if (System.Diagnostics.Debugger.IsAttached)
             {
@@ -31,7 +35,7 @@ public class HelloDomino
                 Console.WriteLine($"Stack Trace: {ex.StackTrace}");
             }
         }
-        
+
     }
 
     private static void solveByGraphs()
@@ -40,9 +44,11 @@ public class HelloDomino
         if (!string.IsNullOrEmpty(input))
         {
             var dominos = parseInputToGraph(input);
-            if(dominos==null){
+            if (dominos == null)
+            {
                 Console.WriteLine("Unable to parse input, please try with valid input again");
-            }else
+            }
+            else
             {
                 //condition 1: Every node has an even degree
                 bool isValidDomino = validateEvenDegrees(dominos);
@@ -50,26 +56,30 @@ public class HelloDomino
                 {
                     //condition 2: Every node is connected to one single cycle
                     var cycle = verifyEulerianCycle(dominos);
-                    if(cycle==null){
+                    if (cycle == null)
+                    {
                         Console.WriteLine("Domino Circle is not possible for the given input.");
-                    }else{
+                    }
+                    else
+                    {
                         var pairs = createDominoPairs(cycle);
                         printPairs(pairs);
                     }
-                    
+
                 }
             }
         }
-        
+
     }
 
     private static int[,] createDominoPairs(List<int> eulerianCycle)
     {
-        int[,] pairs = new int[eulerianCycle.Count, 2];;
-        for(var i=0;i<eulerianCycle.Count;i++){
-            var next=i==eulerianCycle.Count-1?0:i+1;
-            pairs[i,0]=eulerianCycle[i];
-            pairs[i,1]=eulerianCycle[next];
+        int[,] pairs = new int[eulerianCycle.Count, 2]; ;
+        for (var i = 0; i < eulerianCycle.Count; i++)
+        {
+            var next = i == eulerianCycle.Count - 1 ? 0 : i + 1;
+            pairs[i, 0] = eulerianCycle[i];
+            pairs[i, 1] = eulerianCycle[next];
         }
         return pairs;
     }
@@ -100,9 +110,9 @@ public class HelloDomino
         Stack<int> stack = new Stack<int>();
         HashSet<int> visited = new HashSet<int>();
         List<int> dominoChain = new List<int>();
-        
+
         stack.Push(startNode);
-        
+
         while (stack.Count > 0)
         {
             int top = stack.Pop();
@@ -112,7 +122,8 @@ public class HelloDomino
                 dominoChain.Add(top);
                 foreach (var neighbor in dominos[top])
                 {
-                    if (!visited.Contains(neighbor)){
+                    if (!visited.Contains(neighbor))
+                    {
                         stack.Push(neighbor);
                     }
                 }
@@ -144,7 +155,8 @@ public class HelloDomino
     private static Dictionary<int, List<int>> parseInputToGraph(string input)
     {
         string[] pairs = input.Split(' ');
-        if (pairs.Length==0){
+        if (pairs.Length == 0)
+        {
             return null;
         }
         Dictionary<int, List<int>> values = new Dictionary<int, List<int>>();
@@ -155,23 +167,27 @@ public class HelloDomino
             string cleanPair = pairs[i].Trim('[', ']');
 
             string[] numbers = cleanPair.Split('|');
-            
+
             //handling invalid input case
-            if (numbers.Length<2){
+            if (numbers.Length < 2)
+            {
                 return null;
             }
             int key, adjacentVal;
-            int.TryParse(numbers[0],out key);
-            int.TryParse(numbers[1],out adjacentVal);
-            if(key<1 || adjacentVal<1){
+            int.TryParse(numbers[0], out key);
+            int.TryParse(numbers[1], out adjacentVal);
+            if (key < 1 || adjacentVal < 1)
+            {
                 return null;
             }
 
-            if(!values.ContainsKey(key)){
-                values[key]=new List<int>();
+            if (!values.ContainsKey(key))
+            {
+                values[key] = new List<int>();
             }
-            if(!values.ContainsKey(adjacentVal)){
-                values[adjacentVal]=new List<int>();
+            if (!values.ContainsKey(adjacentVal))
+            {
+                values[adjacentVal] = new List<int>();
             }
             values[key].Add(adjacentVal);
             values[adjacentVal].Add(key);
@@ -190,34 +206,41 @@ public class HelloDomino
             {
                 Console.WriteLine("Unable to parse input, please try with valid input again");
             }
-            else{
-                Dictionary<int,int> frequencyDict= new Dictionary<int, int>();
+            else
+            {
+                Dictionary<int, int> frequencyDict = new Dictionary<int, int>();
 
-                for( var i=0;i<dominos.GetLength(0);i++){
-                    if( !frequencyDict.ContainsKey(dominos[i,0])){
-                        frequencyDict[dominos[i,0]]=0;
+                for (var i = 0; i < dominos.GetLength(0); i++)
+                {
+                    if (!frequencyDict.ContainsKey(dominos[i, 0]))
+                    {
+                        frequencyDict[dominos[i, 0]] = 0;
                     }
-                    if( !frequencyDict.ContainsKey(dominos[i,1])){
-                        frequencyDict[dominos[i,1]]=0;
+                    if (!frequencyDict.ContainsKey(dominos[i, 1]))
+                    {
+                        frequencyDict[dominos[i, 1]] = 0;
                     }
-                    frequencyDict[dominos[i,0]]++;
-                    frequencyDict[dominos[i,1]]++;
+                    frequencyDict[dominos[i, 0]]++;
+                    frequencyDict[dominos[i, 1]]++;
                 }
                 bool isValidDomino = true;
-                foreach( var key in frequencyDict){
-                    if(key.Value%2!=0){
+                foreach (var key in frequencyDict)
+                {
+                    if (key.Value % 2 != 0)
+                    {
                         isValidDomino = false;
                         Console.WriteLine("Domino Circle is not possible for the given input.");
                         break;
                     }
                 }
-                if(isValidDomino){
-                    dominos=bruteForceApproach(dominos);
+                if (isValidDomino)
+                {
+                    dominos = bruteForceApproach(dominos);
                     printPairs(dominos);
                 }
             }
         }
-        
+
     }
 
     private static void solveByBruteForce()
@@ -244,50 +267,56 @@ public class HelloDomino
                 }
             }
         }
-        
+
     }
 
     private static string getInput()
-    {   
+    {
         Console.WriteLine("Please enter the input string in this format: [1|2] [2|3] [3|1] ... and then press ENTER");
         string input = Console.ReadLine();
-        if (string.IsNullOrEmpty(input)) 
+        if (string.IsNullOrEmpty(input))
         {
             Console.WriteLine("Input cannot be empty. Please try again.");
             return "";
-        }else{
+        }
+        else
+        {
             return input;
         }
-        
+
     }
 
     private static int[,] bruteForceApproach(int[,] pairs)
     {
-        if( pairs == null || pairs.GetLength(0)==0 || pairs.GetLength(0)==1){
+        if (pairs == null || pairs.GetLength(0) == 0 || pairs.GetLength(0) == 1)
+        {
             return null;
         }
 
-        for( var i = 0;i<pairs.GetLength(0)-1;i++){
+        for (var i = 0; i < pairs.GetLength(0) - 1; i++)
+        {
             int current = i;
-            int next = i+1;
+            int next = i + 1;
             bool hasMatch = false;
-            for( var j = i+1;j<pairs.GetLength(0);j++)
+            for (var j = i + 1; j < pairs.GetLength(0); j++)
             {
                 //checking if current element matches either first or second element of next Domino
                 hasMatch = findMatch(pairs, current, next, j);
-                if( hasMatch ){
+                if (hasMatch)
+                {
                     break;
                 }
 
             }
-            if (!hasMatch){
+            if (!hasMatch)
+            {
                 return null;
             }
         }
         return pairs;
     }
 
-    private static bool findMatch(int[,] pairs, int current, int next,  int j)
+    private static bool findMatch(int[,] pairs, int current, int next, int j)
     {
         bool hasMatch = false;
         if (pairs[current, 1] == pairs[j, 0] || pairs[current, 1] == pairs[j, 1])
@@ -308,7 +337,7 @@ public class HelloDomino
                     return false;
                 }
             }
-            hasMatch = true;  
+            hasMatch = true;
         }
         return hasMatch;
     }
@@ -336,38 +365,45 @@ public class HelloDomino
         var size = pairs.GetLength(0);
         for (int i = 0; i < size; i++)
         {
-            Console.Write("["+ pairs[i, 0] + "|" + pairs[i, 1] + "] ");
+            Console.Write("[" + pairs[i, 0] + "|" + pairs[i, 1] + "] ");
         }
         Console.WriteLine("");
     }
 
-    public static int[,] parseInputToMatrix(string input){
-        try{
-            string[] pairs = input.Split(' ');
-        if (pairs.Length==0){
-            return null;
-        }
-        int[,] values = new int[pairs.Length, 2];
-
-        for (int i = 0; i < pairs.Length; i++)
+    public static int[,] parseInputToMatrix(string input)
+    {
+        try
         {
-            // getting the element i.e. [2|3] -> 2,3
-            string cleanPair = pairs[i].Trim('[', ']');
+            string[] pairs = input.Split(' ');
+            if (pairs.Length == 0)
+            {
+                return null;
+            }
+            int[,] values = new int[pairs.Length, 2];
 
-            string[] numbers = cleanPair.Split('|');
-            //handling invalid input case
-            if (numbers.Length<2){
-                return null;
+            for (int i = 0; i < pairs.Length; i++)
+            {
+                // getting the element i.e. [2|3] -> 2,3
+                string cleanPair = pairs[i].Trim('[', ']');
+
+                string[] numbers = cleanPair.Split('|');
+                //handling invalid input case
+                if (numbers.Length < 2)
+                {
+                    return null;
+                }
+                int.TryParse(numbers[0], out values[i, 0]);
+                int.TryParse(numbers[1], out values[i, 1]);
+                //handling invalid input case
+                if (values[i, 0] < 1 || values[i, 1] < 1)
+                {
+                    return null;
+                }
             }
-            int.TryParse(numbers[0],out values[i,0]);
-            int.TryParse(numbers[1],out values[i,1]);
-            //handling invalid input case
-            if(values[i,0]<1 || values[i,1]<1){                                                                        
-                return null;
-            }
+            return values;
         }
-        return values;
-        }catch(Exception ex){
+        catch (Exception ex)
+        {
             Console.WriteLine("Error occured while parsing input. Please try again.");
             if (System.Diagnostics.Debugger.IsAttached)
             {
@@ -376,6 +412,6 @@ public class HelloDomino
             }
             return null;
         }
-        
+
     }
 }
